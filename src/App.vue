3201f6faa8d7c4ea30cf8932c211286f1ba42911
zihuns <seojh44@uos.ac.kr>
2022-03-09 09:47:27 +0900

    vue3-lifecycle

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>{{ count }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 2
+    }
+  },
+  beforeCreate() {
+    console.log('Before Create!', this.count)
+  },
+  created() {
+    console.log('Created!', this.count)
+  },
+  beforeMount() {
+    console.log('Before Mount!')
+    console.log(document.querySelector('h1'))
+  },
+  mounted() {
+    console.log('Mounted!')
+    console.log(document.querySelector('h1'))
+  }
+}
+</script>
